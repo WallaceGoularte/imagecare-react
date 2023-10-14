107a0574527b731ccbdff40c3054b1b60d73921a
Wallace Goularte <wallace.goularte@hotmail.com>
2023-10-14 20:15:50 -0300

    Implementação da tela de agendamento de exames

--- src/views/Agendamento/index.js
+++ src/views/Agendamento/index.js
@@ -1,22 +1,60 @@
-import React from 'react';
-import * as S from './styles'
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import React, { useState, useEffect } from "react";
+import * as S from "./styles";
+
+import api from "../../services/api";
+
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 
 function Agendamento() {
   return (
     <S.Container>
-      <Header/>
+      <Header />
+      <S.Title>
+        <h3>Agendar Exames</h3>
+      </S.Title>
 
-        <S.Save>
+      <S.Form>
+          <S.Input>
+              <span>Exame</span>
+          </S.Input>
+          <S.Select>
+            <option value="">Selecionar um Exame</option>
+          </S.Select>
+
+          <S.Input>
+              <span>Médico</span>
+          </S.Input>
+          <S.Select>
+            <option value="">Selecionar Médico (a)</option>
+          </S.Select>
+
+          <S.Input>
+              <span>Data</span>
+              <input type="date" placeholder="Data"></input>
+          </S.Input>
+          
+          <S.Spacer />
+
+          <S.Input>
+              <span>Hora</span>
+          </S.Input>
+          <S.Select>
+              <option value="">Selecionar Hora</option>
+          </S.Select>
+
+
+
+          <S.Save>
             <button type="button">SALVAR</button>
-        </S.Save>
-      
-      <Footer/>
-    </S.Container>
+          </S.Save>
 
-  )
+      </S.Form>
+
+      <Footer />
+    </S.Container>
+  );
 }
 
 export default Agendamento;
--- src/views/Agendamento/styles.js
+++ src/views/Agendamento/styles.js
@@ -2,10 +2,81 @@ import styled from 'styled-components';
 
 export const Container = styled.div` 
     width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+
+export const Title = styled.div`
+    width: 100%;
+    border-bottom: 1px solid #295393;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+
+    h3 {
+        color: #295393;
+        position: relative;
+        top: 30px;
+        background: #FFF;
+        padding: 0 30px;
+    }
+
+` 
+
+export const Form = styled.div`
+    width: 50%;
+    height: 500px;
+    margin-bottom: 70px;
+
+
+`
+
+export const Select = styled.select`
+    width: 100%;
+    background: #fff;
+    border: 1px solid #295393;
+    font-size: 16px;
+    padding: 10px;
+    color: #295393;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+
+    &:hover {
+        opacity: 0.7;
+    }
+
+`
+
+export const Input = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 5px;
+
+    span {
+        color: #707070;
+        margin: 5px 0;
+    }
+
+    input {
+        font-size: 16px;
+        padding: 15px;
+        border: 1px solid #295393;
+        border-bottom: 1px solid #295393;
+        cursor: pointer;
+
+    }
+
 `
 
 export const Save = styled.div`
     width: 100%;
+    margin-top: 20px;
 
     button {
         width: 100%;
@@ -14,6 +85,16 @@ export const Save = styled.div`
         font-size: 20px;
         color: #fff;
         font-weight: bold;
+        padding: 20px;
+        border-radius: 30px;
+        cursor: pointer;
     }
 
+    
+
+
+`
+
+export const Spacer = styled.div`
+  height: 20px;
 `
